b3c613442e7793c28aef154e13cb659b26210976
jefffranko7@gmail.com <jefffranko7@gmail.com>
2021-02-20 02:25:02 +0530

    Commit 4

--- RaysCoursesApplication/Controllers/HomeController.cs
+++ RaysCoursesApplication/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace RaysCoursesApplication.Controllers
         HelperApi _api = new HelperApi();
         List<Course> courses;
         List<CourseViewModel> courseViewModels;
+        List<string> dropdownResult;
         Course course;
         University university;
 
@@ -33,7 +34,7 @@ namespace RaysCoursesApplication.Controllers
             return View();
         }
 
-        public async Task<IActionResult> GetTableData()
+        public async Task<IActionResult> GetTableData(string? category)
         {
             courses = new List<Course>();
             courseViewModels = new List<CourseViewModel>();
@@ -41,12 +42,16 @@ namespace RaysCoursesApplication.Controllers
             HttpClient client = _api.Initial();
             HttpResponseMessage res1 = await client.GetAsync("api/Courses");
 
-
             if (res1.IsSuccessStatusCode)
             {
                 var result1 = res1.Content.ReadAsStringAsync().Result;
                 courses = JsonConvert.DeserializeObject<List<Course>>(result1);
-                
+
+                if(category != null && category != "All")
+                {
+                    courses = courses.Where(x => x.Ccategory == category).ToList();
+                }
+
                 foreach(var i in courses)
                 {
                     HttpResponseMessage res2 = await client.GetAsync("api/Universities/" + i.UniRefId);
@@ -57,6 +62,27 @@ namespace RaysCoursesApplication.Controllers
                     courseViewModels.Add ( new CourseViewModel{ Cid = i.Cid, UniId = i.UniRefId, UniName = university.UniName, Cname = i.Cname, CdateOfIntake = i.CdateOfIntake, Cyears = i.Cyears, Cfee = i.Cfee, UniImgPath = university.UniImgPath });
                 }
                 return Json(new { data = courseViewModels });
+            }
+
+            return Json(new { data = false });
+        }
+
+        public async Task<IActionResult> GetCategoriesDropdown()
+        {
+            courses = new List<Course>();
+            dropdownResult = new List<string>();
+
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("api/Courses");
+
+
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                courses = JsonConvert.DeserializeObject<List<Course>>(result);
+
+                dropdownResult = courses.Select(o => o.Ccategory).Distinct().ToList();
+                return Json(new { data = dropdownResult });
             }
 
             return Json(new { data = false });
